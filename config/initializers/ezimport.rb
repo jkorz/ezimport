e11ae563c1e9c0609226e83b430fe436c3432b49
Joe Korzeniewski <trogdor33@gmail.com>
2013-02-15 22:28:35 -0500

    Remove old files

--- config/initializers/ezimport.rb
+++
@@ -1,8 +0,0 @@
-require 'ezimport'
-
-#EzImport.models - Array of names of models that will be imported/exported with ezimport:all and ezimport:export:all
-#EzImport.models = ['UserType', 'Permission']
-EzImport.models = []
-
-#EzImport.xmlpath - Path where the XML files will be stored Default: "db/ezimport"
-#EzImport.xmlpath = "db/ezimport"
